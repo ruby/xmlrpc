07132e307d6d3abc9de78ab7ad452e36ef53c35c
Sutou Kouhei <kou@clear-code.com>
2020-11-22 07:21:36 +0900

    test: suppress access log

--- test/test_server.rb
+++ test/test_server.rb
@@ -9,7 +9,15 @@ require 'xmlrpc/server'
 module TestXMLRPC
 class TestXMLRPCServer < Test::Unit::TestCase
   def test_port
-    s = XMLRPC::Server.new(0, '127.0.0.1', 1, StringIO.new)
+    s = nil
+    begin
+      stdout, $stdout = $stdout, StringIO.new
+      stderr, $stderr = $stderr, StringIO.new
+      s = XMLRPC::Server.new(0, '127.0.0.1', 1)
+    ensure
+      $stdout = stdout
+      $stderr = stderr
+    end
     refute_equal(0, s.port, 'Selected random port')
 
     s.add_handler('test.add') { |a, b| a + b }
