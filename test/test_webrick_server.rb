2b2ae322cd9b001f3c568e2b5cb9d03717f987e5
Herwin <herwinw@users.noreply.github.com>
2025-05-15 14:53:44 +0200

    Various layout cleanups in test_webrick_server
    These remarks originate from the review of the tests of the CGI server,
    but since most of that code was copied from this file, clean this up
    here as well.
    If we ever want to use shared logic for these files, it's preferable to
    keep the differences to a minimum.

--- test/test_webrick_server.rb
+++ test/test_webrick_server.rb
@@ -24,11 +24,11 @@ class Test_Webrick < Test::Unit::TestCase
       super(req, res)
     }
 
-    s.add_handler("test.add") do |a,b|
+    s.add_handler("test.add") do |a, b|
       a + b
     end
 
-    s.add_handler("test.div") do |a,b|
+    s.add_handler("test.div") do |a, b|
       if b == 0
         raise XMLRPC::FaultException.new(1, "division by zero")
       else
@@ -65,24 +65,23 @@ class Test_Webrick < Test::Unit::TestCase
 
   def test_client_server
     # NOTE: I don't enable SSL testing as this hangs
-    [false].each do |use_ssl|
-      option = setup_http_server_option(use_ssl)
-      with_server(option, method(:create_servlet)) {|addr|
-        @s = XMLRPC::Client.new3(:host => addr.ip_address, :port => addr.ip_port, :use_ssl => use_ssl)
-        @s.user = 'admin'
-        @s.password = 'admin'
-        silent do
-          do_test
-        end
-        @s.http.finish
-        @s = XMLRPC::Client.new3(:host => addr.ip_address, :port => addr.ip_port, :use_ssl => use_ssl)
-        @s.user = '01234567890123456789012345678901234567890123456789012345678901234567890123456789'
-        @s.password = 'guest'
-        silent do
-          do_test
-        end
-        @s.http.finish
-      }
+    use_ssl = false
+    option = setup_http_server_option(use_ssl)
+    with_server(option, method(:create_servlet)) do |addr|
+      @s = XMLRPC::Client.new3(:host => addr.ip_address, :port => addr.ip_port, :use_ssl => use_ssl)
+      @s.user = 'admin'
+      @s.password = 'admin'
+      silent do
+        do_test
+      end
+      @s.http.finish
+      @s = XMLRPC::Client.new3(:host => addr.ip_address, :port => addr.ip_port, :use_ssl => use_ssl)
+      @s.user = '01234567890123456789012345678901234567890123456789012345678901234567890123456789'
+      @s.password = 'guest'
+      silent do
+        do_test
+      end
+      @s.http.finish
     end
   end
 
@@ -97,37 +96,38 @@ class Test_Webrick < Test::Unit::TestCase
 
   def do_test
     # simple call
-    assert_equal 9, @s.call('test.add', 4, 5)
+    assert_equal(9, @s.call('test.add', 4, 5))
 
     # fault exception
     assert_raise(XMLRPC::FaultException) { @s.call('test.div', 1, 0) }
 
     # fault exception via call2
     ok, param = @s.call2('test.div', 1, 0)
-    assert_equal false, ok
-    assert_instance_of XMLRPC::FaultException, param
-    assert_equal 1, param.faultCode
-    assert_equal 'division by zero', param.faultString
+    assert_equal(false, ok)
+    assert_instance_of(XMLRPC::FaultException, param)
+    assert_equal(1, param.faultCode)
+    assert_equal('division by zero', param.faultString)
 
     # call2 without fault exception
     ok, param = @s.call2('test.div', 10, 5)
-    assert_equal true, ok
-    assert_equal param, 2
+    assert_equal(true, ok)
+    assert_equal(param, 2)
 
     # introspection
-    assert_equal ["test.add", "test.div", "system.listMethods", "system.methodSignature", "system.methodHelp"], @s.call("system.listMethods")
+    assert_equal(["test.add", "test.div", "system.listMethods", "system.methodSignature", "system.methodHelp"],
+                 @s.call("system.listMethods"))
 
     # default handler (missing handler)
     ok, param = @s.call2('test.nonexisting')
-    assert_equal false, ok
+    assert_equal(false, ok)
     assert_equal(-99, param.faultCode)
 
     # default handler (wrong number of arguments)
     ok, param = @s.call2('test.add', 1, 2, 3)
-    assert_equal false, ok
+    assert_equal(false, ok)
     assert_equal(-99, param.faultCode)
 
     # multibyte characters
-    assert_equal "あいうえおかきくけこ", @s.call('test.add', "あいうえお", "かきくけこ")
+    assert_equal("あいうえおかきくけこ", @s.call('test.add', "あいうえお", "かきくけこ"))
   end
 end
